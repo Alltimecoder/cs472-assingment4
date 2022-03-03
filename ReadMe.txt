47a4d871482dddbd50534c6864038cd9be26716c
sayal <adhsayal@gmail.com>
2022-03-03 09:22:49 -0600

    console form data

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,1 +1,4 @@
 Hosted in: https://alltimecoder.github.io/cs472-assingment4/
+
+ChangeLog1:
+-> added js to display form data on console
---
+++ js/form.js
@@ -0,0 +1,6 @@
+document.querySelector('form').addEventListener('submit', (e) => {
+    const data = Object.fromEntries(new FormData(e.target).entries());
+    console.log(data);
+    event.preventDefault();
+  });
+  
